0db47c5175c38b270c040b98ea6d4e0bfdd8731a
jimrobert796 <jimmyroberto270@gmail.com>
2026-05-23 16:55:31 -0600

    Configuracion y sincronizacion con el proyecto de firebase y android studio

--- miPrimeraApp/app/build.gradle.kts
+++ miPrimeraApp/app/build.gradle.kts
@@ -41,4 +41,11 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:34.13.0"))
+    implementation(platform("com.google.firebase:firebase-database"))
+    implementation(platform("com.google.firebase:firebase-messaging"))
+    implementation(platform("com.google.firebase:firebase-storage"))
+    implementation(platform("com.firebaseui:firebase-ui-storage:9.0.0"))
+
 }
